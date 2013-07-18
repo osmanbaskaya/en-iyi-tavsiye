067700c3c88ccc4e3be20357e86a78b1efee0a69
OsmanBaskaya <thyme.exe@gmail.com>
2013-07-18 19:45:09 +0300

    404 handled

--- image-dl.py
+++ image-dl.py
@@ -7,21 +7,23 @@ __author__ = "Osman Baskaya"
 from imdbdata.models import Item
 import urllib2
 from time import sleep
-import os
+import os, sys
 from django.core.paginator import Paginator
 
 
 items = Item.objects.all()
 paginator = Paginator(items, 100)
-path = 'imdbdata/static/img/item-imgs/'
-
+path = '/data/imdbdata_images/item-imgs/'
 
 for i in xrange(1, paginator.num_pages + 1):
     for item in paginator.page(i).object_list:
         if not os.path.exists("{}{}.jpg".format(path, item.id)):
             print item.id, item.img
-            image = urllib2.urlopen(item.img).read()
-            with open(path + str(item.id) + '.jpg', 'w') as f:
-                f.write(image)
-            sleep(0.5)
+            try:
+                image = urllib2.urlopen(item.img).read()
+                with open(path + str(item.id) + '.jpg', 'w') as f:
+                    f.write(image)
+                sleep(1)
+            except:
+                print >> sys.stderr, "{} couldn't downloaded | 404".format(item.id)
 
