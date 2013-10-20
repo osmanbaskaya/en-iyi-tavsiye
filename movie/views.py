ca70815f13b1f9495bb8f1d3acc6b3dae229a1b4
OsmanBaskaya <thyme.exe@gmail.com>
2013-10-20 23:34:02 +0300

    some changes

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -8,10 +10,15 @@ import json
 from webservice import WebService
 import random
 from random import sample
+from itertools import cycle
 
 #constants
 context = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
+
+# bio ornekleri. 1.5'ten sonra silinecek
+bio=["Gezi direnişçisi", "Gençlik ve spor bakanı", "Akademisyen", "Yazılım Mühendisi", "Mimar", 
+"Avukat @ Başkaya Hukuk Bürosu", "Okutman @ Bahçeşehir Üniversitesi"]
 
 @login_required(login_url='/login/')
 def user_popover(request):
@@ -26,18 +33,22 @@ def user_popover(request):
 
     return render(request, context + '/user_popover.html',
             {'context':context,'user':user,'sim_perc':sim_perc,'f':f})
+
 @login_required(login_url='/login/')
 def search(request):
-    items = Item.objects.filter( name__icontains=request.GET.get('q'))[0:20]
-    rows=[]
+    items = Item.objects.filter( name__icontains=request.GET.get('q'))[:10]
+    users = User.objects.filter( username__icontains=request.GET.get('q'))
+
+    rows = []
     for item in items:
         try:
             r=Rating.objects.get(user=request.user,item = item)
             rows.append((item,r))
         except Rating.DoesNotExist:
             rows.append((item,None))
-    return render(request, context + '/ratings.html',{'context':context,'rlist':range(1,6),
-        'rows':rows})
+    users_bios = zip(users, cycle(bio))
+    return render(request, context + '/search.html',{'context':context,'rlist':range(1,6),
+        'users_bios':users_bios, 'rows':rows, 'act':'item'})
 
 @login_required(login_url='/login/')
 def reclist(request):
@@ -150,8 +161,8 @@ def rate(request):
         Action.objects.filter(user=u,what='rate',gen_id=i.pk).delete()
         rating.delete()
         rating=None
-
     if r>0:
+        Rating.objects.filter(user=u,item=i).delete()
         rating = Rating.objects.create(user=u,item=i,rating=r)
         Action.objects.create(user=request.user,what='rate',
                 gen_id=i.pk)
